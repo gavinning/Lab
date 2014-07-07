d3250a671ddd4e09f25e6e944fe2a7e371f4186a
ningyubo <ningyubo@gmail.com>
2014-07-07 17:11:19 +0800

    test

--- src/test.js
+++ src/test.js
@@ -1,15 +1,20 @@
-var lib = require('./lib');
+// var lib = require('./lib');
 
-var date = new Date().getTime();
+// var date = new Date().getTime();
 
-var files = lib.dir('/Users/iLinco/Downloads', [], /\.zip/)
+// var files = lib.dir('/Users/iLinco/Downloads', [], /\.zip/)
 
-var now = new Date().getTime();
+// var now = new Date().getTime();
 
-// console.log(now - date)
-console.log(files.file.length)
-// console.log(files.folder.length)
-
-
+// // console.log(now - date)
+// console.log(files.file.length)
+// // console.log(files.folder.length)
 
 
+var http = require('http');
+var fs = require('fs');
+var Canvas = require('canvas');
+var Image = Canvas.Image;
+
+
+console.log(Canvas)
